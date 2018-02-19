17dfff19cea37dae6128839b92ed64c701017801
PSabuStryka <parkershuck1@yahoo.com>
2018-02-18 23:43:08 -0600

    Update content.js

--- content.js
+++ content.js
@@ -4,6 +4,7 @@ chrome.runtime.onMessage.addListener(
     if( request.message === "clicked_browser_action" ) {
       var firstHref = $("a[href^='http']").eq(0).attr("href");
       console.log(firstHref);
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
     }
   }
 );
