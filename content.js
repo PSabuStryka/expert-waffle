f87ad530ef77ec28857d5659878e2bb31dada771
PSabuStryka <parkershuck1@yahoo.com>
2018-02-18 23:39:48 -0600

    Update content.js
    revised to add in message listening from background.js

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 // content.js
-alert("Hello from your Chrome extension!")
-
-// content.js
-var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-console.log(firstHref);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      console.log(firstHref);
+    }
+  }
+);
