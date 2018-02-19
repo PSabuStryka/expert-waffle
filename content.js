515e4a0ac8fd0d2b6b1dd7e180c1beb1b182d46c
PSabuStryka <parkershuck1@yahoo.com>
2018-02-19 17:04:48 -0600

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,12 @@
 // content.js
-var firstHref;
+var passVar="";
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      firstHref = $("a[href^='http']").eq(0).attr("href");
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
       console.log(firstHref);
+      passVar = firstHref;
       chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
     }
   }
