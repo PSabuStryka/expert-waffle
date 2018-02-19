fcf030ec7646d930018bace3cdf2b396dde361bb
PSabuStryka <parkershuck1@yahoo.com>
2018-02-19 16:59:39 -0600

    Update content.js

--- content.js
+++ content.js
@@ -1,8 +1,9 @@
 // content.js
+var firstHref;
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      firstHref = $("a[href^='http']").eq(0).attr("href");
       console.log(firstHref);
       chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
     }
