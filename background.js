fa1e5c0581b080ff76d5ec0f16e754ea2cf222fa
PSabuStryka <parkershuck1@yahoo.com>
2018-02-18 23:46:06 -0600

    Update background.js

--- background.js
+++ background.js
@@ -8,3 +8,12 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
+// Listens for url from background.js then open url
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "open_new_tab" ) {
+      chrome.tabs.create({"url": request.url});
+    }
+  }
+);
+
